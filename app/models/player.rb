29a0f0a257a3c118a51e6068d07e49c0b3d2ff1d
Bhuvna Prabhu <bbhuvnaprabhu@gmail.com>
2021-09-21 16:33:10 +0530

    Add player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,12 @@
 class Player < ApplicationRecord
-  has_many :rounds, dependent: :destroy
+  has_many :rounds, -> { order(created_at: :asc) }, dependent: :destroy
   belongs_to :game
 
   after_create :create_rounds
+
+  def total_score
+    rounds.pluck(:scores).flatten.collect(&:to_i).sum
+  end
 
   private
 
